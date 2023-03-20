58ad07278e5f1fb75001374a1a707aa27d35e70b
Pamella Pereto <pamellapereto@gmail.com>
2023-03-20 19:40:03 -0300

    Update ContentHome.js

--- components/ContentHome.js
+++ components/ContentHome.js
@@ -29,41 +29,81 @@ export default function ContentHome() {
           <Text>R$ 34.99</Text>
         </View>
 
-        <View style={design.product1}>
-          <Image
-            style={design.imgProduct}
-            source={{
-              uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
-            }}
-          />
-
+             <View style={design.product1}>
+          <ScrollView horizontal={true}>
+            <Image
+              style={design.imgProduct}
+              source={{
+                uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
+              }}
+            />
+            <Image
+              style={design.imgProduct}
+              source={{
+                uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
+              }}
+            />
+            <Image
+              style={design.imgProduct}
+              source={{
+                uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
+              }}
+            />
+          </ScrollView>
           <Text>Shorts jeans</Text>
           <Text>R$ 34.99</Text>
         </View>
 
-        <View style={design.product1}>
-          <Image
-            style={design.imgProduct}
-            source={{
-              uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
-            }}
-          />
-
+             <View style={design.product1}>
+          <ScrollView horizontal={true}>
+            <Image
+              style={design.imgProduct}
+              source={{
+                uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
+              }}
+            />
+            <Image
+              style={design.imgProduct}
+              source={{
+                uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
+              }}
+            />
+            <Image
+              style={design.imgProduct}
+              source={{
+                uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
+              }}
+            />
+          </ScrollView>
           <Text>Shorts jeans</Text>
           <Text>R$ 34.99</Text>
         </View>
 
-        <View style={design.product1}>
-          <Image
-            style={design.imgProduct}
-            source={{
-              uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
-            }}
-          />
-
+             <View style={design.product1}>
+          <ScrollView horizontal={true}>
+            <Image
+              style={design.imgProduct}
+              source={{
+                uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
+              }}
+            />
+            <Image
+              style={design.imgProduct}
+              source={{
+                uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
+              }}
+            />
+            <Image
+              style={design.imgProduct}
+              source={{
+                uri: 'https://img.ltwebstatic.com/gspCenter/goodsImage/2023/1/31/5445485991_1005024/6D7E499E7D135E4399939E179E59A656_thumbnail_600x.jpg',
+              }}
+            />
+          </ScrollView>
           <Text>Shorts jeans</Text>
           <Text>R$ 34.99</Text>
-        </View>
+        </View>   
+
       </View>
     </View>
   );
@@ -85,11 +125,18 @@ const design = StyleSheet.create({
     justifyContent: 'center',
     textAlign: 'center',
     alignItems: 'center',
-    width: '50%',
+    width: '34%',
     height: '50%',
+    marginRight: 100,
+    marginLeft: 11,
+    marginTop: -10,
   },
 
   imgProduct: {
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    resizeMode: 'cover',
+    margin: 10,
     width: 110,
     height: 150,
     borderRadius: 10,
